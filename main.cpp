4f8a7915b6e2b914b8a2c61b99d2a25d522c00f6
taiandrew <31713490+taiandrew@users.noreply.github.com>
2025-11-10 20:51:24 -0800

    Skeleton

---
+++ main.cpp
@@ -0,0 +1,36 @@
+// COMSC 210 | Lab 32 | 10 NOV 2025
+// IDE: VS Code
+
+#include <algorithm>
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <string>
+
+#include "Car.h"
+
+using namespace std;
+
+// Constants
+const int INIT_SIZE = 2;
+
+// Fn prototypes
+int prob();
+
+// Main
+int main() {
+
+    // Set seed
+    srand(time(0));
+
+
+
+
+    return 0;
+}
+
+// Functions
+int prob() {
+    // Random int 1-100
+    return rand() % 100 + 1;
+}
